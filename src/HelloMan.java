2e59f31cccbc19236249312aa85e27be81626495
KrisD1977 <derela@student.wszib.edu.pl>
2020-11-15 08:41:01 +0100

    Initial commit

---
+++ src/HelloMan.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class HelloMan {
+
+    public static void main(String[] args) {
+
+
+        System.out.print("Jak masz na imię?");
+
+        Scanner scanner = new Scanner(System.in);
+
+        String n = scanner.next();
+
+
+        System.out.print("Cześć, " + n);
+    }
+}
