e237cc920550c2223527491251e7df91f41c2adb
KrisD1977 <derela@student.wszib.edu.pl>
2020-12-07 22:31:01 +0100

    nauka 07.12.2020

--- src/ChristmasTreeAscii.java
+++ src/ChristmasTreeAscii.java
@@ -1,34 +1,69 @@
 import java.util.Random;
 
-public class    ChristmasTreeAscii {
-
+public class ChristmasTreeAscii {
+    
     public static void main(String[] args) {
-        int rows = 35;
 
-        char[] signs = {'+', '.', '*', '~', '^', 'o'};
+        if (args.length > 0) {
 
-        char [] randomSigns = new char[rows+1];
+            Integer rows = Integer.valueOf(args[0]);
 
-        for (int i = 0; i < rows+1 ; i++) {
-            Random los = new Random();
-            int a = los.nextInt(6);
-            randomSigns [i] = signs[a];
+            char[] signs = {'+', '.', '*', '~', '^', 'o'};
 
-        }
+            char[] randomSigns = new char[rows + 1];
 
-
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < (2*i +1) ; j++) {
+            for (int i = 0; i < rows + 1; i++) {
                 Random los = new Random();
-                int a = los.nextInt(randomSigns.length);
+                int a = los.nextInt(6);
+                randomSigns[i] = signs[a];
 
-                for (int k = 0; k < (40-i) && j==0; k++) {
-                    System.out.print(" ");
-                }
-                
-            System.out.print(randomSigns [a] );
             }
-            System.out.println(" ");
+
+
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j < (2 * i + 1); j++) {
+                    Random los = new Random();
+                    int a = los.nextInt(randomSigns.length);
+
+                    for (int k = 0; k < (40 - i) && j == 0; k++) {
+                        System.out.print(" ");
+                    }
+
+                    System.out.print(randomSigns[a]);
+                }
+                System.out.println(" ");
+
+            }
+        } else {
+            int rows = 18;
+
+            char[] signs = {'+', '.', '*', '~', '^', 'o'};
+
+            char[] randomSigns = new char[rows + 1];
+
+            for (int i = 0; i < rows + 1; i++) {
+                Random los = new Random();
+                int a = los.nextInt(6);
+                randomSigns[i] = signs[a];
+
+            }
+
+
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j < (2 * i + 1); j++) {
+                    Random los = new Random();
+                    int a = los.nextInt(randomSigns.length);
+
+                    for (int k = 0; k < (40 - i) && j == 0; k++) {
+                        System.out.print(" ");
+                    }
+
+                    System.out.print(randomSigns[a]);
+                }
+                System.out.println(" ");
+
+            }
+
 
         }
 
